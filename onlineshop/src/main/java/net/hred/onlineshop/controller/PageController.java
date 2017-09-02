c04dfda73d3e9eac16325e3ab3575ab4d6537f88
Orest Hredil <Hredil.Orest@gmail.com>
2017-09-02 03:15:19 +0300

    updating project

--- onlineshop/src/main/java/net/hred/onlineshop/controller/PageController.java
+++ onlineshop/src/main/java/net/hred/onlineshop/controller/PageController.java
@@ -1,17 +1,27 @@
 package net.hred.onlineshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.hred.shopbackend.dao.CategoryDAO;
+
 @Controller
 public class PageController {
 
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
 	@RequestMapping(value = {"", "/home", "/index"})
 	public ModelAndView index() {
 		
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
+		
+		//passing the list of categories
+		mv.addObject("categories",categoryDAO.list());
+		
 		mv.addObject("userClickHome", true);
 		
 		return mv;
---
+++ shopbackend/src/main/java/net/hred/shopbackend/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package net.hred.shopbackend.dao;
+
+import java.util.List;
+
+import net.hred.shopbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	List<Category> list();
+	
+}
